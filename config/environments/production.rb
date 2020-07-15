adaeeefc43baac5bca0090583f4c51ac35753bda
Ryan Crispin Heneise <ryan@boosterstage.net>
2020-07-15 08:47:25 -0500

    Rename project

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "linkburner_production"
+  # config.active_job.queue_name_prefix = "selfdestructlink_production"
 
   config.action_mailer.perform_caching = false
 
